418dcd076e432abb5a9f35c8a453f90ff7539ae3
MADAN KUMAR K <MADAN KUMAR K@LAPTOP-IQ9B34OU>
2022-11-21 17:38:37 +0530

    modify

--- src/test/java/practice/create_test.java
+++ src/test/java/practice/create_test.java
@@ -4,6 +4,7 @@ public class create_test {
 	public static void main(String[] args) {
 		System.out.println("created");
 		System.out.println("update");
+		System.out.println("modify");
 		System.out.println("delete");
 		
 	}
